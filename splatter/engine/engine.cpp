9c8f733c00ba1f2891dc6999814db713c41172c2
Nick Wierzbowski <nwierzbow@gmail.com>
2025-08-17 10:41:43 -0400

    build: finish build system and test with cpp files

---
+++ splatter/engine/engine.cpp
@@ -0,0 +1,7 @@
+#include "engine.h"
+#include <iostream> // For printing to the console
+
+void say_hello_from_cpp() {
+    // This message will appear in the terminal you launch Blender from.
+    std::cout << "Hello from the C++ Engine!" << std::endl;
+}
---
+++ splatter/engine/engine.h
@@ -0,0 +1,4 @@
+#pragma once // Prevents the header from being included multiple times
+
+// This is the function we want to expose to Python via Cython.
+void say_hello_from_cpp();
