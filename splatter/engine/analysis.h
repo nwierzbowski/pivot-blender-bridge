fa4bce3cdf9d45dd30c3a877e842ca3b1e58b942
Nick Wierzbowski <nwierzbow@gmail.com>
2025-08-31 20:08:18 -0400

    style: formatting

--- splatter/engine/analysis.h
+++ splatter/engine/analysis.h
@@ -6,22 +6,24 @@
 #include <vector>
 #include <cstdint>
 
-std::vector<Vec2> calc_base_convex_hull(const std::vector<Vec3>& verts, BoundingBox3D full_box);
+std::vector<Vec2> calc_base_convex_hull(const std::vector<Vec3> &verts, BoundingBox3D full_box);
 
-struct SliceData {
+struct SliceData
+{
     float area;
     Vec2 centroid;
     float mid_z;
 };
 
-struct COGResult {
+struct COGResult
+{
     Vec3 overall_cog;
     std::vector<SliceData> slices;
 };
 
-COGResult calc_cog_volume_edges_intersections(const Vec3* verts,
+COGResult calc_cog_volume_edges_intersections(const Vec3 *verts,
                                               uint32_t vertCount,
-                                              const uVec2i* edges,
+                                              const uVec2i *edges,
                                               uint32_t edgeCount,
                                               BoundingBox3D full_box,
                                               float slice_height);
