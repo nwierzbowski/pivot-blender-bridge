9b7f2044d918fadceaaac3a9b314fb70e7279da7
Nick Wierzbowski <nwierzbow@gmail.com>
2025-09-24 19:55:30 -0400

    refactor: rearchitect cython structure to be more extensible

--- cython/operators/prepare/bridge.pyx
+++ cython/operators/prepare/bridge.pyx
@@ -5,7 +5,7 @@ from mathutils import Quaternion
 import numpy as np
 import time
 
-from . import selection_utils, shm_utils, transform_utils
+from ..shared import selection_utils, shm_utils, transform_utils
 
 # -----------------------------
 # Main
